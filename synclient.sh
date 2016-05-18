ceaae26f09ae4fa704a96fdabdd47bd2c1890a3c
hobe <hobe@hobe-glaptop6.roam.corp.google.com>
2016-05-18 13:13:30 -0700

    Add synclient command setup for X1

---
+++ synclient.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+synclient TapButton1=0
+synclient TapButton2=0
+synclient TapButton3=0
+synclient ClickFinger1=1
+synclient ClickFinger2=3
+synclient ClickFinger3=2
+synclient LockedDrags=0
+synclient VertTwoFingerScroll=1
+synclient HorizTwoFingerScroll=0
+synclient VertEdgeScroll=0
+synclient HorizEdgeScroll=0
