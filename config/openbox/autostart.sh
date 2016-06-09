7ffe957aec7f020362f3e61f6f10a056fae75f94
hobe <hobe@hobe-glaptop.roam.corp.google.com>
2016-06-09 10:17:53 -0700

    update submodules
    add autostart.local support

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -3,7 +3,7 @@
 eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
 export SSH_AUTH_SOCK
 gsettings set com.canonical.desktop.interface scrollbar-mode normal &
-xinput set-prop 11 "Device Enabled" 0 &
+xinput set-prop 11 "Device Enabled" 1 &
 xscreensaver &
 xmodmap ~/.Xmodmap &
 xset r rate 220 30 &
@@ -16,3 +16,6 @@ xfce4-panel &
 #$HOME/bin/keyserver &
 feh --no-xinerama --bg-scale images/Backgrounds/mandolux-basil-lr-1920.jpg &
 #xinput set-button-map "Kingsis Peripherals Evoluent VerticalMouse 4" 1 3 8 4 5 6 7 8 2 10 11 12 13 14
+if [[ -e ~/.config/openbox/autostart.local.sh ]]; then
+  ~/.config/openbox/autostart.local.sh
+fi
