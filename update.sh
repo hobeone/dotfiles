360c60a884945e0aa045820a1195072c05e7e399
Daniel Hobe <hobe@gmail.com>
2017-10-27 14:32:47 -0700

    Update install and change nginx plugin

--- update.sh
+++ update.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -ex
 git pull
 git submodule update --init --remote
+git submodule foreach git pull origin master
+
 git status
-./install_go_tools.sh
-pushd vim/bundle/tern_for_vim
-npm install
-popd
+if hash go 2>/dev/null; then
+  ./install_go_tools.sh
+else
+  echo "No go executable found."
+fi
+
+if hash npm 2>/dev/null; then
+  pushd vim/bundle/tern_for_vim
+  npm install
+  popd
+else
+  echo "npm not found"
+fi
