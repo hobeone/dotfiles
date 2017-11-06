66c3bc3a9596fc7d30ee5cabcc968ed89d42624a
Daniel Hobe <hobe@hobe2.mtv.corp.google.com>
2017-11-06 10:23:33 -0800

    Remove key handler

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -13,7 +13,6 @@ xfsettingsd --no-daemon &
 xfce4-volumed --no-daemon &
 xfce4-power-manager --no-daemon &
 xfce4-panel &
-#$HOME/bin/keyserver &
 feh --no-xinerama --bg-scale images/Backgrounds/mandolux-basil-lr-1920.jpg &
 #xinput set-button-map "Kingsis Peripherals Evoluent VerticalMouse 4" 1 3 8 4 5 6 7 8 2 10 11 12 13 14
 if [[ -e ~/.config/openbox/autostart.local.sh ]]; then
