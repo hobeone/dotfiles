d2e943828db1504faee2c725a326037c9494501f
Daniel Hobe <hobe@gmail.com>
2017-11-08 09:15:28 -0800

    Update autostart and update plugins

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -3,9 +3,10 @@
 eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
 export SSH_AUTH_SOCK
 gsettings set com.canonical.desktop.interface scrollbar-mode normal &
-xinput set-prop 11 "Device Enabled" 1 &
-xscreensaver &
-xss-lock -- xscreensaver-command -lock &
+
+# Enable touchpad
+#xinput set-prop 11 "Device Enabled" 1 &
+
 xmodmap ~/.Xmodmap &
 xset r rate 220 30 &
 xset dpms 300 600 600 &
@@ -14,8 +15,12 @@ xfsettingsd --no-daemon &
 xfce4-volumed --no-daemon &
 xfce4-power-manager --no-daemon &
 xfce4-panel &
-feh --no-xinerama --bg-scale images/Backgrounds/mandolux-basil-lr-1920.jpg &
 #xinput set-button-map "Kingsis Peripherals Evoluent VerticalMouse 4" 1 3 8 4 5 6 7 8 2 10 11 12 13 14
+if [ ! -f ~/.config/openbox/noscreenlock ]; then
+  xscreensaver &
+  xss-lock -- xscreensaver-command -lock &
+fi
+
 if [[ -e ~/.config/openbox/autostart.local.sh ]]; then
   ~/.config/openbox/autostart.local.sh
 fi
