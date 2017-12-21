eb2f2779e93d98d8c00c5fdbc5a4be8116dcb68d
Daniel Hobe <hobe@gmail.com>
2017-12-21 09:54:19 -0800

    Update plugins Override oh-my-zsh url escaping Update tmux config for new version

---
+++ zsh_custom/lib/misc.zsh
@@ -0,0 +1,28 @@
+# Override oh-my-zsh turning on url quoting which is annoying
+# This is a copy of misc.zsh from oh-my-zsh which ommits that code
+
+## jobs
+setopt long_list_jobs
+
+## pager
+env_default PAGER 'less'
+env_default LESS '-R'
+
+## super user alias
+alias _='sudo'
+alias please='sudo'
+
+## more intelligent acking for ubuntu users
+if which ack-grep &> /dev/null; then
+  alias afind='ack-grep -il'
+else
+  alias afind='ack -il'
+fi
+
+# only define LC_CTYPE if undefined
+if [[ -z "$LC_CTYPE" && -z "$LC_ALL" ]]; then
+	export LC_CTYPE=${LANG%%:*} # pick the first entry from LANG
+fi
+
+# recognize comments
+setopt interactivecomments
