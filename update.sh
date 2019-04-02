4176f10b02e92ac5e995b4e1fe3be8822bfdceb4
Daniel Hobe <hobe@gmail.com>
2019-04-02 10:10:43 -0700

    Update modules and update script

--- update.sh
+++ update.sh
@@ -1,19 +1,10 @@
 #!/bin/bash -ex
 git pull
-git submodule update --init --remote
-git submodule foreach git pull origin master
+git submodule update --init --recursive
 
 git status
 if hash go 2>/dev/null; then
   ./install_go_tools.sh
 else
   echo "No go executable found."
-fi
-
-if hash npm 2>/dev/null; then
-  pushd vim/bundle/tern_for_vim
-  npm install
-  popd
-else
-  echo "npm not found"
 fi
