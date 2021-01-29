d89afe48a7ac1067f8a9a83f379602dc01c14e70
Daniel Hobe <hobe@gmail.com>
2021-01-29 11:16:52 -0800

    Set undermouse new window behavior Run xmodmap on startup update install_udev to reload rules and install inotify-tools match keyboard by manufacturer and model

--- install_udev.sh
+++ install_udev.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # Copy udev scripts over to handle USB KVM switch switching keyboards
 # needs to be run as root
+#
+# run with ECHO=echo to debug
 
 if hash inotifywait 2>/dev/null; then
   echo "Found inotifywait command installed"
 else
-  echo "Didn't find inotifywait, please install.  apt install inotify-tools"
+  echo "Didn't find inotifywait, installing"
+  $ECHO apt install inotify-tools
 fi
 
 $ECHO cp -v udev/99-keyboard.rules /etc/udev/rules.d/99-keyboard.rules
 $ECHO cp -v bin/keyboard-udev /usr/local/bin/keyboard-udev
+$ECHO udevadm control --reload-rules
