d427eb2ccc597d58c76065f061b339ce961ae9ec
Daniel Hobe <hobe@google.com>
2022-02-03 12:38:18 -0800

    Add lenovo laptop performance tuning & monitoring scripts

--- bin/performance.sh
+++ bin/performance.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash -ex
 for d in 0 1 2 3 4 5 6 7 ; do
   echo performance | sudo tee /sys/devices/system/cpu/cpu${d}/cpufreq/scaling_governor
 done
 
+
+# Requires intel-gpu-tools package
+# Set's GPU clock to max
+sudo intel_gpu_frequency -m
+
+# Set's power limit to max (on Lenovo X1 Gen 9)
+echo 64000000 | sudo tee /sys/devices/virtual/powercap/intel-rapl-mmio/intel-rapl-mmio\:0/constraint_0_power_limit_u
