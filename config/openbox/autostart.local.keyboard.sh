cda2361ce04ab7182305857455fcc59a978e1d38
Daniel Hobe <hobe@gmail.com>
2022-12-16 08:42:19 -0800

    Add mouse button settings Update ohmyzsh and utilites

--- config/openbox/autostart.local.keyboard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-~/bin/keyboard-settings &
-~/bin/file-inotify /tmp/keyboard.lock ~/bin/keyboard-settings &  # Triggered by udev rule
-
-~/dotfiles/synclient.sh &
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 git pull --recurse-submodules --verbose
-
+git submodule update --init --recursive
+git submodule foreach git pull --recurse-submodules origin master
 git status --verbose
