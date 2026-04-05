1c3492a2a354fbde36b7cdb672b6517105b80ef5
Daniel Hobe <hobe@gmail.com>
2026-04-05 14:12:02 -0700

    small fix to keyboard settings script

--- home/.config/openbox/autostart.local.sh
+++ home/.config/openbox/autostart.local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 ~/bin/keyboard-settings &
-~/bin/file-inotify /tmp/keyboard.lock ~/bin/keyboard-settings &  # Triggered by udev rule
+#~/bin/file-inotify /tmp/keyboard.lock ~/bin/keyboard-settings &  # Triggered by udev rule
 
 ~/bin/synclient.sh &
 
